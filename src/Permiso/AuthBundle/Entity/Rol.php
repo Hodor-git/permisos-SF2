3888d2f4bd7d23b0c17af5271e44fd9287a94023
javimontes <lorathlon@gmail.com>
2012-11-15 19:14:12 +0100

    Creada la base de datos y añadidos los getters y seters a las entidades.

--- src/Permiso/AuthBundle/Entity/Rol.php
+++ src/Permiso/AuthBundle/Entity/Rol.php
@@ -19,7 +19,45 @@ class Rol implements RoleInterface
     protected $id;
  
     /**
-     * @ORM\Column(name="nombre", type="string", length="255")
+     * @ORM\Column(name="nombre", type="string", length=255)
      */
     protected $nombre;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Rol
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+    
+    public function getRole() 
+    {
+       return $this->nombre; 
+    }
 }
--- src/Permiso/AuthBundle/Entity/Usuario.php
+++ src/Permiso/AuthBundle/Entity/Usuario.php
@@ -19,17 +19,17 @@ class Usuario implements UserInterface
     protected $id;
  
     /**
-     * @ORM\Column(name="usuario", type="string", length="255")
+     * @ORM\Column(name="usuario", type="string", length=255)
      */
     protected $usuario;
  
     /**
-     * @ORM\Column(name="password", type="string", length="255")
+     * @ORM\Column(name="password", type="string", length=255)
      */
     protected $password;
  
     /**
-     * @ORM\Column(name="salt", type="string", length="255")
+     * @ORM\Column(name="salt", type="string", length=255)
      */
     protected $salt;
  
@@ -41,4 +41,119 @@ class Usuario implements UserInterface
      * )
      */
     protected $roles;
+
+    public function getSalt() 
+    {
+        return $this->salt;
+    }
+
+    
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->roles = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set usuario
+     *
+     * @param string $usuario
+     * @return Usuario
+     */
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+    
+        return $this;
+    }
+
+    /**
+     * Get usuario
+     *
+     * @return string 
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+    
+    public function getUsername() 
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Usuario
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    
+        return $this;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return Usuario
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    
+        return $this;
+    }
+
+    /**
+     * Add roles
+     *
+     * @param Permiso\AuthBundle\Entity\Rol $roles
+     * @return Usuario
+     */
+    public function addRole(\Permiso\AuthBundle\Entity\Rol $roles)
+    {
+        $this->roles[] = $roles;
+    
+        return $this;
+    }
+
+    /**
+     * Remove roles
+     *
+     * @param Permiso\AuthBundle\Entity\Rol $roles
+     */
+    public function removeRole(\Permiso\AuthBundle\Entity\Rol $roles)
+    {
+        $this->roles->removeElement($roles);
+    }
+
+    public function getPassword() 
+    {
+        return $this->password;
+    }
+
+    public function getRoles() 
+    {
+        return $this->roles->toArray();
+    }
+    
+    public function eraseCredentials() {
+        
+    }
 }
