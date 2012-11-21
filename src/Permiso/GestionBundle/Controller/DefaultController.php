128f18c783fde71b5d32f27e41e12edd9f53fa96
javimontes <lorathlon@gmail.com>
2012-11-21 10:47:34 +0100

    Limpieza de controladores, cambio de inicio.html.twig a menuPrincipal.html.twig y limpieza interna de los routing.yml

--- src/Permiso/GestionBundle/Controller/DefaultController.php
+++ src/Permiso/GestionBundle/Controller/DefaultController.php
@@ -4,94 +4,30 @@ namespace Permiso\GestionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Permiso\GestionBundle\Entity\Vacaciones;
-use Permiso\GestionBundle\Entity\TipoPermiso;
-use Permiso\GestionBundle\Entity\Permiso;
-use Permiso\GestionBundle\Entity\Gestor;
-use Permiso\GestionBundle\Entity\Categoria;
-use Permiso\GestionBundle\Entity\Empleado;
-
 class DefaultController extends Controller
 {
+    /**
+     * Si no hay usuario en sesión muestra el index. En caso
+     * contrario muestra el menú principal de la aplicación.
+     */
     public function indexAction()
     {
-        /*
-        $vacaciones  = new Vacaciones;
+        $usuarioEnSesion = $this->getUser();
         
-        $hoy = new \DateTime("now");
+        if(is_null($usuarioEnSesion)) {
+            return $this->render('PermisoGestionBundle:Default:index.html.twig');
+        }
         
-        $vacaciones->setEmpleado(1);
-        $vacaciones->setDiasPedidos(20);
-        $vacaciones->setFechaEntrada($hoy);
-        $vacaciones->setFechaGestion($hoy);
-        $vacaciones->setFinalizada(true);
-        $vacaciones->setDenegada(false);
-        $vacaciones->setObservaciones('Probando observaciones en BDD');
-
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($vacaciones);
-        $em->flush();
-        
-        $em = $this->getDoctrine()->getEntityManager();
-        
-        $categoria = new Categoria();
-        
-        $categoria->setNombre('empleado');
-        $categoria->setDiasVacaciones(20);
-        
-        $gestor = $em->getRepository('PermisoGestionBundle:Gestor');
-        
-        $pepito = $gestor->findBy(array('username' => 'pepe'));
-        
-        
-        $manolo = new Empleado();
-        
-        $manolo->setUsername('manolo');
-        $manolo->setPassword('gafotas');
-        $manolo->setSalt('asd312asdas23');
-        $manolo->setEmail('manolo@lolailo.com');
-        $manolo->setCategoria($categoria);
-        $manolo->setGestor($pepito[0]);
-        
-        $em->persist($categoria);
-        $em->persist($manolo);
-        
-        $em->flush();*/
-        
-        return $this->render('PermisoGestionBundle:Default:index.html.twig'); /*, array('gestor' => var_dump($pepito)*/
+        return $this->render('PermisoGestionBundle:Default:menuPrincipal.html.twig');
     }
     
-    public function permisoAction()
-    {
-        $tipoPermiso = new TipoPermiso();
-        
-        $hoy = new \DateTime("now");
-        
-        $tipoPermiso->setNombre('paternidad');
-        $tipoPermiso->setDuracion(10);
-        
-        $permiso = new Permiso();
-        
-        $permiso->setEmpleado(2);
-        $permiso->setFechaEntrada($hoy);
-        $permiso->setFechaGestion($hoy);
-        $permiso->setFinalizada(false);
-        $permiso->setDenegada(false);
-        $permiso->setObservaciones('probando las entidades de permiso');
-        $permiso->setTipoPermiso($tipoPermiso);
-        
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($tipoPermiso);
-        $em->persist($permiso);
-        
-        $em->flush();
-        
-        return $this->render('PermisoGestionBundle:Default:index.html.twig', array());
-    }
-    
+    /**
+     * Controlador asignado al menú principal de la aplicación que
+     * aparece una vez logueado el usuario
+     */
     public function inicioAction()
-    {
-        return $this->render('PermisoGestionBundle:Default:inicio.html.twig', array());
+    {       
+        return $this->render('PermisoGestionBundle:Default:menuPrincipal.html.twig', array());
     }
     
     
